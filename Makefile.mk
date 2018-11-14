80cfa3ed3556498a2e0005588e43ed49e9701524
Cristian Borja <cm.borja10@uniandes.edu.co>
2018-11-14 07:24:57 -0500

    Add files via upload

--- Makefile.mk
+++ Makefile.mk
@@ -1,11 +1,17 @@
-Listing_122: Listing_122.c 
-	mpicc Listing_122.c -o Listing_122
-	mpirun -np 4 ./Listing_122
-	
-Listing_123: Listing_123.c 
-	mpicc Listing_123.c -o Listing_123
-	mpirun -np 4 ./Listing_123
+all: L1 L2
+
+L1: Listing_122.x
+	mpirun -np 4 ./Listing_122.x
+
+L2: Listing_123.x
+	mpirun -np 4 ./Listing_123.x
+
+Listing_122.x: Listing_122.c
+	mpicc Listing_122.c -o Listing_122.x
+
+Listing_123.x: Listing_123.c
+	mpicc Listing_123.c -o Listing_123.x
 
 clean:
-	rm -f Listing_122
-	rm -f Listing_123
+	rm -f L1
+	rm -f L2
